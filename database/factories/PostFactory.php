dd4da0f5aef25d1ee860d96d9419862130b3b31a
sakanaction317 <danki.karate9602@gmail.com>
2024-10-22 20:39:56 +0900

    メソッドを修正

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,7 +18,7 @@ class PostFactory extends Factory
     {
         return [
             'title' => fake()->realText(15),
-            'content' => fake()->realText,
+            'content' => fake()->realText(),
             'user_id' => 1
         ];
     }
